52ab10102f6e63a3bf68aaf5ad849f5dc7883e3e
otakaaaa <sotaka3104@gmail.com>
2024-07-28 02:46:21 +0900

    #3 ヘッダー作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OTAKA SPACES",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="jp">
-      <body className={inter.className}>
+      <body>
         <Header />
         {children}
         <Footer />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
-import './Header.module.css';
+import styles from './Header.module.css';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <>
-      <div>Header</div>
+      <header className={styles.header}>
+        <div className={styles.logo}>OTAKASPACE</div>
+        <div className={styles.links}>
+          <Link href={'/about'} className={styles.link}>OTAKAとは</Link>
+          <Link href={'/services'} className={styles.link}>事業内容</Link>
+          <Link href={'/achievements'} className={styles.link}>実績</Link>
+          <Link href={'/contact'} className={styles.link}>お問い合わせ</Link>
+          <Link href={'/news'} className={styles.link}>ニュース</Link>
+        </div>
+      </header>
     </>
   );
 }
