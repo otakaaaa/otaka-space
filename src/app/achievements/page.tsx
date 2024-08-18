3a6e5a22d2cebf268097ccd21db7c1496c3d8286
htk-otaka <satoshi.otaka@haveatalk.jp>
2024-08-18 17:26:18 +0900

    Revert "ディレクトリ構成変更"

---
+++ src/app/achievements/page.tsx
@@ -0,0 +1,9 @@
+import './page.module.css';
+
+export default function Achievements() {
+  return (
+    <>
+      <div>Achievements</div>
+    </>
+  );
+}
--- src/types/Footer/Footer.ts
+++
@@ -1,4 +0,0 @@
-export type FooterLinkType = {
-    title: string;
-    link: string;
-}
