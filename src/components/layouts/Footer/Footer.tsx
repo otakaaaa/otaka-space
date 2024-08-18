072c9cb00752d843379f8d750dc276c2c3c14823
satoshi.otaka <satoshi.otaka@haveatalk.jp>
2024-08-18 17:35:18 +0900

    Footer等の中身修正

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -6,7 +6,26 @@ import { FooterLinks } from '@/constants/Footer/Footer';
 export default function Footer() {
   return (
     <>
-      <div>Footer</div>
+      <footer className={styles.footerArea}>
+        <div className={styles.footerWrapper}>
+          <div className={styles.logo}>OTAKA SPACE</div>
+          <div>
+            <div className={styles.footerLinks}>
+              {
+                FooterLinks.map((link, index) => (
+                  <Fragment key={index}>
+                    <Link href={link.link} className={styles.footerLink}>{link.title}</Link>
+                  </Fragment>
+                ))
+              }
+            </div>
+            <div className={styles.localeArea}>
+              言語：<span>日本語</span> | <span>English</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.copyRight}>&copy; 2024 OTAKA SPACE</div>
+      </footer>
     </>
   );
 }
