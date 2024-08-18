90a604dba657951d892129462cbf3efad514f43e
satoshi.otaka <satoshi.otaka@haveatalk.jp>
2024-08-18 17:16:36 +0900

    ディレクトリ構成変更

---
+++ src/components/layouts/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import styles from './Footer.module.css';
+import Link from 'next/link';
+import { FooterLinks } from '@/constants/Footer/Footer';
+
+export default function Footer() {
+  return (
+    <>
+      <div>Footer</div>
+    </>
+  );
+}
---
+++ src/types/Footer/Footer.ts
@@ -0,0 +1,4 @@
+export type FooterLinkType = {
+    title: string;
+    link: string;
+}
