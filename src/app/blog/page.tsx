3c1bcdafe9b2464bf756f80fe1c5da85bcd99d2f
satoshi.otaka <satoshi.otaka@haveatalk.jp>
2024-09-16 13:23:37 +0900

    ブログ一覧：タグデザイン調整

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -41,7 +41,7 @@ export default async function Blog() {
           <Divider my={'md'} />
           <DivPC>
             {uniqueTags && uniqueTags.length > 0 && uniqueTags.map((tag, index) => (
-              <Button w={'fit-content'} fz={'14px'} radius={'50px'} bg={'#404C96'} key={index} className={css({ _hover: {backgroundColor: '#fff !important', color: '#404C96 !important', border: '1px solid #404C96 !important'} })}>
+              <Button key={index} className={tagStyle}>
                 {tag.label}
               </Button>
             ))}
@@ -62,3 +62,19 @@ export default async function Blog() {
     </>
   );
 }
+
+const tagStyle = css({
+  width: 'fit-content',
+  fontSize: '14px',
+  borderRadius: '50px !important',
+  backgroundColor: '#404C96 !important',
+  marginBottom: '20px',
+  _hover: {
+    backgroundColor: '#fff !important',
+    color: '#404C96 !important',
+    border: '1px solid #404C96 !important',
+  },
+  _last: {
+    marginBottom: 0,
+  }
+})
