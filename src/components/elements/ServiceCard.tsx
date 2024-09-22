4b1377d8d6ea3cf6710498324986d7e5b618df45
satoshi.otaka <satoshi.otaka@haveatalk.jp>
2024-09-22 13:41:47 +0900

    サービス：一覧修正、詳細削除

--- src/components/elements/ServiceCard.tsx
+++ src/components/elements/ServiceCard.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { Card, Image, Text, Button, Stack, Box } from '@mantine/core';
+import { Card, Group, Text, Menu, ActionIcon, Image, SimpleGrid, rem } from '@mantine/core';
+import { IconExternalLink } from '@tabler/icons-react';
 import { css } from '../../../styled-system/css';
-import { useRouter } from 'next/navigation';
 import { ServiceDetails } from '@/types/Service/Service';
+import Link from 'next/link';
 import parse from 'html-react-parser';
 
 interface MyCardType {
@@ -11,37 +12,37 @@ interface MyCardType {
 }
 
 export default function ServiceCard(props: MyCardType) {
-    const router = useRouter();
-    const handleClick = (id: number) => {
-        router.push(`/services/detail/${id}`);
-    }
-
     return (
-        <Card className={css({width: '100%', md: { minWidth: '280px' }})} shadow="sm" padding="lg" radius="md" withBorder mb={30}>
-            <Card.Section>
-                <Image
-                    src={props.items.image.url_org}
-                    alt={props.items.image.desc}
-                    className={css({width: '100%', height: 'auto'})}
-                />
+        <Card withBorder shadow="sm" radius="md" className={css({width: '100%', md: { minWidth: '230px', width: '48%' }})}>
+            <Card.Section withBorder inheritPadding py="xs">
+                <Group justify="space-between">
+                    <Text fw={500}>{props.items.subject}</Text>
+                    <Menu withinPortal position="bottom-end" shadow="sm">
+                        <ActionIcon variant="subtle" color="gray">
+                            <Link href={props.items.link} target='_blank'>
+                                <IconExternalLink style={{ width: rem(16), height: rem(16) }} />
+                            </Link>
+                        </ActionIcon>
+                    </Menu>
+                </Group>
             </Card.Section>
 
+            <Text mt="sm" c="dimmed" size="sm">
+                {parse(props.items.contents)}
+            </Text>
 
-            <Stack mt="md" mb="xs" gap={'3px'}>
-                <Text fz={'14px'}>{props.items.ymd}</Text>
-                <Text fw={'bold'}>{props.items.subject}</Text>
-            </Stack>
+            <Card.Section mt="sm">
+                <Image src={props.items.image.url_org} alt={props.items.image.desc} />
+            </Card.Section>
 
-            {
-                props.items.contents &&
-                <Box fz={'sm'} c={'dimmed'}>
-                    {parse(props.items.contents)}
-                </Box>
-            }
-
-            <Button onClick={() => handleClick(props.items.topics_id)} color="#404C96" fullWidth mt="md" radius="md">
-                詳細を見る
-            </Button>
+            <Card.Section inheritPadding mt="sm" pb="md">
+                <SimpleGrid cols={3}>
+                {props.items.sub_images.length > 0 &&
+                props.items.sub_images.map((image, index) => (
+                    <Image src={image.url_org} alt={image.desc} key={index} radius="sm" />
+                ))}
+                </SimpleGrid>
+            </Card.Section>
         </Card>
     );
 }
